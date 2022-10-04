10333e313204085a58e545c4b9c6a9e0eb4c469c
Honganh2609 <mazic004@gmail.com>
2022-10-04 11:43:36 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # do-an
+# do-an
---
+++ js/script.js
@@ -0,0 +1,16 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle("open");
+  }
+
+  product = {
+    "id":"1",
+    "name":"iPhone 14 Pro Max",
+    "regular_price":"35990000",
+    "sale_price":"28990000",
+    "image":"",
+    "phone": {
+        "home":"028341932",
+        "hand":"0918764526",
+        "work":"028156392"
+    }
+};
